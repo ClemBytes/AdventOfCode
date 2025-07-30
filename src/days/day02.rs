5d447e0438a8cc6d8f513bfc58de5e845c9b5594
Clémentine Scohy <clembytes@proton.me>
2025-07-30 21:52:48 +0200

    DAY02: init

---
+++ src/days/day02.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+pub fn run() {
+    println!("------- DAY02 -------");
+    let input = fs::read_to_string("inputs/input_day02")
+        .expect("Unable to read input!");
+    day02_part1(&input);
+    day02_part2(&input);
+}
+
+fn day02_part1(_input: &String) {
+
+}
+
+fn day02_part2(_input: &String) {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod days;
 
 fn main() {
     days::day01::run();
+    days::day02::run();
 }
