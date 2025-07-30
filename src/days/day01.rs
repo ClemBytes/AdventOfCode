8c19cb41f145151e2969be0dc6566e2890a473b9
Clémentine Scohy <clembytes@proton.me>
2025-07-30 21:45:31 +0200

    DAY01: part2 OK

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,14 +1,16 @@
 use std::fs;
 
 pub fn run() {
-    println!("-------------------");
-    println!("------ DAY01 ------");
-    println!("-------------------");
-    day01_part1();
-    day01_part2();
+    println!("---------------------");
+    println!("------- DAY01 -------");
+    println!("---------------------");
+    let input = fs::read_to_string("inputs/input_day01")
+        .expect("Unable to read input!");
+    day01_part1(&input);
+    day01_part2(&input);
 }
 
-fn day01_part1() {
+fn day01_part1(input: &String) {
     // Exemple tests
     let test = String::from("(())");
     assert_eq!(get_floor(&test), 0);
@@ -29,14 +31,18 @@ fn day01_part1() {
     let test = String::from(")())())");
     assert_eq!(get_floor(&test), -3);
 
-    let input = fs::read_to_string("inputs/input_day01")
-        .expect("Unable to read input!");
     assert_eq!(get_floor(&input), 138);
     println!("> DAY01 - part 1: OK!");
 }
 
-fn day01_part2() {
-    println!("TODO day01 - part2");
+fn day01_part2(input: &String) {
+    let test = String::from(")");
+    assert_eq!(basement_when(&test), 1);
+    let test = String::from("()())");
+    assert_eq!(basement_when(&test), 5);
+
+    assert_eq!(basement_when(&input), 1771);
+    println!("> DAY01 - part 2: OK!");
 }
 
 fn get_floor(instructions: &String) -> i32 {
@@ -50,4 +56,22 @@ fn get_floor(instructions: &String) -> i32 {
     }
     floor
     // We can do easier by counting number of '(', number of ')' and substract…
+}
+
+fn basement_when(instructions: &String) -> u32 {
+    let mut floor = 0;
+    let mut position = 1;
+    for parenthesis in instructions.chars() {
+        match parenthesis {
+            '(' => { floor += 1; },
+            ')' => { floor -= 1; },
+            _   => { println!("Unkwnow character {parenthesis}! Should be ( or )."); },
+        }
+        if floor == -1 {
+            return position;
+        }
+        position += 1;
+    }
+    println!("Never found basement!");
+    position
 }
