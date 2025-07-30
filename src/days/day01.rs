38148bb52d0529120f9fcbcac8a2eecf613ac4d5
Clémentine Scohy <clembytes@proton.me>
2025-07-30 21:53:52 +0200

    DAY01: comments

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,9 +1,7 @@
 use std::fs;
 
 pub fn run() {
-    println!("---------------------");
     println!("------- DAY01 -------");
-    println!("---------------------");
     let input = fs::read_to_string("inputs/input_day01")
         .expect("Unable to read input!");
     day01_part1(&input);
@@ -31,21 +29,25 @@ fn day01_part1(input: &String) {
     let test = String::from(")())())");
     assert_eq!(get_floor(&test), -3);
 
+    // Solve puzzle
     assert_eq!(get_floor(&input), 138);
     println!("> DAY01 - part 1: OK!");
 }
 
 fn day01_part2(input: &String) {
+    // Exemple tests
     let test = String::from(")");
     assert_eq!(basement_when(&test), 1);
     let test = String::from("()())");
     assert_eq!(basement_when(&test), 5);
 
+    // Solve puzzle
     assert_eq!(basement_when(&input), 1771);
     println!("> DAY01 - part 2: OK!");
 }
 
 fn get_floor(instructions: &String) -> i32 {
+    // Part 1
     let mut floor = 0;
     for parenthesis in instructions.chars() {
         match parenthesis {
@@ -59,6 +61,7 @@ fn get_floor(instructions: &String) -> i32 {
 }
 
 fn basement_when(instructions: &String) -> u32 {
+    // Part 2
     let mut floor = 0;
     let mut position = 1;
     for parenthesis in instructions.chars() {
