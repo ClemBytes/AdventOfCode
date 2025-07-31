37099f7e90bc34ea819eba931acb28e29c92f5a9
Clémentine Scohy <clembytes@proton.me>
2025-07-31 12:44:35 +0200

    DAY03: return directly size

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -13,38 +13,35 @@ pub fn run() {
 
 fn day03_part1(input: &str) {
     // Exemple tests
-    assert_eq!(nb_visited_houses(get_visited_houses(">")), 2);
-    assert_eq!(nb_visited_houses(get_visited_houses("^>v<")), 4);
-    assert_eq!(nb_visited_houses(get_visited_houses("^v^v^v^v^v")), 2);
+    assert_eq!(get_visited_houses(">"), 2);
+    assert_eq!(get_visited_houses("^>v<"), 4);
+    assert_eq!(get_visited_houses("^v^v^v^v^v"), 2);
 
     // Solve puzzle
     // println!(
     //     "Result part 1: {}",
-    //     nb_visited_houses(get_visited_houses(&input))
+    //     get_visited_houses(&input)
     // );
-    assert_eq!(nb_visited_houses(get_visited_houses(&input)), 2081);
+    assert_eq!(get_visited_houses(&input), 2081);
     println!("> DAY03 - part 1: OK!");
 }
 
 fn day03_part2(input: &str) {
     // Exemple tests
-    assert_eq!(nb_visited_houses(get_visited_houses_with_robot("^v")), 3);
-    assert_eq!(nb_visited_houses(get_visited_houses_with_robot("^>v<")), 3);
-    assert_eq!(
-        nb_visited_houses(get_visited_houses_with_robot("^v^v^v^v^v")),
-        11
-    );
+    assert_eq!(get_visited_houses_with_robot("^v"), 3);
+    assert_eq!(get_visited_houses_with_robot("^>v<"), 3);
+    assert_eq!(get_visited_houses_with_robot("^v^v^v^v^v"), 11);
 
     // Solve puzzle
     // println!(
     //     "Result part 2: {}",
-    //     nb_visited_houses(get_visited_houses_with_robot(&input))
+    //     get_visited_houses_with_robot(&input)
     // );
-    assert_eq!(nb_visited_houses(get_visited_houses_with_robot(&input)), 2341);
+    assert_eq!(get_visited_houses_with_robot(&input), 2341);
     println!("> DAY03 - part 2: OK!");
 }
 
-fn get_visited_houses(input: &str) -> HashSet<(i32, i32)> {
+fn get_visited_houses(input: &str) -> usize {
     let mut visited_houses = HashSet::new();
     let mut current_location = (0, 0);
     visited_houses.insert(current_location);
@@ -58,10 +55,10 @@ fn get_visited_houses(input: &str) -> HashSet<(i32, i32)> {
         }
         visited_houses.insert(current_location);
     }
-    visited_houses
+    visited_houses.len()
 }
 
-fn get_visited_houses_with_robot(input: &str) -> HashSet<(i32, i32)> {
+fn get_visited_houses_with_robot(input: &str) -> usize {
     let mut visited_houses = HashSet::new();
     let mut santa = (0, 0);
     let mut robot = (0, 0);
@@ -81,9 +78,5 @@ fn get_visited_houses_with_robot(input: &str) -> HashSet<(i32, i32)> {
         }
         visited_houses.insert(*person);
     }
-    visited_houses
-}
-
-fn nb_visited_houses(visited_houses: HashSet<(i32, i32)>) -> usize {
     visited_houses.len()
 }
