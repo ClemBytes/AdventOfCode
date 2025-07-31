0a8b531a6bd75fc85b72bb1e806f196bbd23de33
Clémentine Scohy <clembytes@proton.me>
2025-07-31 09:11:48 +0200

    DAY02: part2 OK

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -33,12 +33,26 @@ fn day02_part1(input: &Vec<Box>) {
     for new_box in input {
         s += new_box.wrapping_paper();
     }
+    // println!("Result part 1: {s}");
     assert_eq!(s, 1606483);
     println!("> DAY02 - part 1: OK!");
 }
 
-fn day02_part2(_input: &Vec<Box>) {
-    println!("TODO - part 2");
+fn day02_part2(input: &Vec<Box>) {
+    // Exemple tests
+    let test_box = Box::read_box("2x3x4");
+    assert_eq!(test_box.ribbon(), 34);
+    let test_box = Box::read_box("1x1x10");
+    assert_eq!(test_box.ribbon(), 14);
+
+    // Solve puzzle
+    let mut s: u32 = 0;
+    for new_box in input {
+        s += new_box.ribbon();
+    }
+    // println!("Result part 2: {s}");
+    assert_eq!(s, 3842356);
+    println!("> DAY02 - part 2: OK!");
 }
 
 struct Box {
@@ -72,5 +86,25 @@ impl Box {
     fn wrapping_paper(&self) -> u32 {
         let sum: u32 = self.get_side_areas().iter().sum();
         2 * sum + self.smallest_side_area()
+    }
+
+    fn cubic(&self) -> u32 {
+        &self.length * &self.width * &self.height
+    }
+
+    fn get_side_perimeters(&self) -> Vec<u32> {
+        vec![
+            &self.length * 2 + &self.width  * 2,
+            &self.length * 2 + &self.height * 2,
+            &self.width  * 2 + &self.height * 2,
+        ]
+    }
+
+    fn smallest_side_perimeter(&self) -> u32 {
+        self.get_side_perimeters().iter().copied().min().unwrap()
+    }
+    
+    fn ribbon(&self) -> u32 {
+        self.smallest_side_perimeter() + self.cubic()
     }
 }
