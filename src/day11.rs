07383a0be7abafc311cc77cf06999292285b6f19
Clémentine Scohy <clembytes@proton.me>
2025-08-02 19:54:57 +0200

    DAY11: init

---
+++ src/day11.rs
@@ -0,0 +1,35 @@
+#[test]
+fn test() {
+    run();
+}
+
+pub fn run() {
+    println!("------- DAY11 -------");
+    let input = "vzbxkghb";
+
+    day11_part1(input);
+    day11_part2(input);
+}
+
+
+fn day11_part1(_input: &str) {
+    println!("TODO - part1");
+    // Exemple tests
+    // assert_eq!(, 0);
+
+    // Solve puzzle
+    // println!("Result part 1: {}");
+    // assert_eq!(, );
+    // println!("> DAY11 - part 1: OK!");
+}
+
+fn day11_part2(_input: &str) {
+    println!("TODO - part2");
+    // Exemple tests
+    // assert_eq!(, 0);
+
+    // Solve puzzle
+    // println!("Result part 2: {}");
+    // assert_eq!(, );
+    // println!("> DAY11 - part 2: OK!");
+}
