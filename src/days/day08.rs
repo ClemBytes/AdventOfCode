75b0a50552fbaaa9aa206301bb1632b503b4c60b
Clémentine Scohy <clembytes@proton.me>
2025-08-02 08:34:08 +0200

    DAY08: init

---
+++ src/days/day08.rs
@@ -0,0 +1,32 @@
+pub fn run() {
+    println!("------- DAY08 -------");
+    let _input = read_input("inputs/input_day08");
+    day08_part1();
+    day08_part2();
+}
+
+fn read_input(_path: &str) {
+
+}
+
+fn day08_part1() {
+    println!("TODO - part1");
+    // Exemple tests
+    // assert_eq!(, 0);
+
+    // Solve puzzle
+    // println!("Result part 1: {}");
+    // assert_eq!(, );
+    // println!("> DAY08 - part 1: OK!");
+}
+
+fn day08_part2() {
+    println!("TODO - part2");
+    // Exemple tests
+    // assert_eq!(, 0);
+
+    // Solve puzzle
+    // println!("Result part 2: {}");
+    // assert_eq!(, );
+    // println!("> DAY08 - part 2: OK!");
+}
