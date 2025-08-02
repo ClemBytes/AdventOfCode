eb1f2f1b463da38f3df353257757d8c6c9f18fe7
Clémentine Scohy <clembytes@proton.me>
2025-08-02 15:04:39 +0200

    DAY09: part1 OK (finally!)

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,4 +1,7 @@
-use std::fs;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 pub fn run() {
     println!("------- DAY09 -------");
@@ -11,28 +14,101 @@ pub fn run() {
     day09_part2(&example, &input);
 }
 
-fn parse(raw_input: &String) -> Vec<_> {
-    let mut yyy: Vec<_> = vec![];
-    for _line in raw_input.lines() {
-        // TODO
-        // yyy.push(line);
+fn parse(raw_input: &str) -> (HashMap<(&str, &str), u32>, HashSet<&str>) {
+    let mut distances: HashMap<(&str, &str), u32> = HashMap::new();
+    let mut cities_names: HashSet<&str> = HashSet::new();
+    for line in raw_input.lines() {
+        let infos: Vec<&str> = line.split(" = ").collect();
+        let (prefix, dist): (&str, &str) = (infos[0], infos[1]);
+        let cities: Vec<&str> = prefix.split(" to ").collect();
+        let dist: u32 = dist.parse().unwrap();
+        distances.insert((cities[0], cities[1]), dist);
+        distances.insert((cities[1], cities[0]), dist);
+        cities_names.insert(cities[0]);
+        cities_names.insert(cities[1]);
     }
-    yyy
+    (distances, cities_names)
 }
 
+fn get_distance(city1: &str, city2: &str, distances: HashMap<(&str, &str), u32>) -> u32 {
+    let couple = (city1, city2);
+    *distances.get(&couple).unwrap()
+}
 
-fn day09_part1(_example: &Vec<_>, _input: &Vec<_>) {
-    println!("TODO - part1");
+fn recursive_search(
+    distances: HashMap<(&str, &str), u32>,
+    cities_names: Vec<&str>,
+    current_city: &str,
+) -> u32 {
+    if cities_names.is_empty() {
+        return 0;
+    }
+    let mut min_dist = u32::MAX;
+    for city in &cities_names {
+        let cities_name_reduced = cities_names
+            .iter()
+            .copied()
+            .filter(|&x| x != *city)
+            .collect();
+        let total_dist = get_distance(current_city, city, distances.clone())
+            + recursive_search(distances.clone(), cities_name_reduced, city);
+        if total_dist < min_dist {
+            min_dist = total_dist;
+        }
+    }
+    min_dist
+}
+
+fn search(distances: HashMap<(&str, &str), u32>, cities_names: Vec<&str>) -> u32 {
+    let mut min_dist = u32::MAX;
+    for city in &cities_names {
+        let cities_name_reduced = cities_names
+            .iter()
+            .copied()
+            .filter(|&x| x != *city)
+            .collect();
+        let total_dist = recursive_search(distances.clone(), cities_name_reduced, city);
+        if total_dist < min_dist {
+            min_dist = total_dist;
+        }
+    }
+    min_dist
+}
+
+fn day09_part1(
+    example: &(HashMap<(&str, &str), u32>, HashSet<&str>),
+    input: &(HashMap<(&str, &str), u32>, HashSet<&str>),
+) {
     // Exemple tests
-    // assert_eq!(, 605);
+    let (example_distances, example_cities_names) = example;
+    let example_cities_names: Vec<&str> = example_cities_names.iter().cloned().collect();
+    // println!(
+    //     "Example part1 : {}",
+    //     search(example_distances.clone(), example_cities_names.clone())
+    // );
+    assert_eq!(
+        search(example_distances.clone(), example_cities_names.clone()),
+        605
+    );
 
     // Solve puzzle
-    // println!("Result part 1: {}");
-    // assert_eq!(, );
-    // println!("> DAY09 - part 1: OK!");
+    let (input_distances, input_cities_names) = input;
+    let input_cities_names: Vec<&str> = input_cities_names.iter().cloned().collect();
+    // println!(
+    //     "Result part 1: {}",
+    //     search(input_distances.clone(), input_cities_names.clone())
+    // );
+    assert_eq!(
+        search(input_distances.clone(), input_cities_names.clone()),
+        141
+    );
+    println!("> DAY09 - part 1: OK!");
 }
 
-fn day09_part2(_example: &Vec<_>, _input: &Vec<_>) {
+fn day09_part2(
+    _example: &(HashMap<(&str, &str), u32>, HashSet<&str>),
+    _input: &(HashMap<(&str, &str), u32>, HashSet<&str>),
+) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
