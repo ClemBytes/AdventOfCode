5e51157b5cf88012b17970c199266c9bfeadeff2
Clémentine Scohy <clembytes@proton.me>
2025-08-05 18:18:55 +0200

    DAY17: part1 OK

--- src/day17.rs
+++ src/day17.rs
@@ -7,36 +7,63 @@ fn test() {
 
 pub fn run() {
     println!("------- DAY17 -------");
-    let example = fs::read_to_string("inputs/example_day17").expect("Unable to read input!");
-    let example = parse(&example);
+    let mut example: Vec<u32> = vec![];
+    example.push(20);
+    example.push(15);
+    example.push(10);
+    example.push(5);
+    example.push(5);
+
     let input = fs::read_to_string("inputs/input_day17").expect("Unable to read input!");
-    let input = parse(&input);
+    let mut input = parse(&input);
 
-    day17_part1(&example, &input);
-    day17_part2(&example, &input);
+    day17_part1(&mut example, &mut input);
+    day17_part2(&mut example, &mut input);
 }
 
-fn parse(raw_input: &String) -> Vec<_> {
-    let mut yyy: Vec<_> = vec![];
-    for _line in raw_input.lines() {
-        // TODO
-        // yyy.push(line);
+const EGGNOG_EXAMPLE: u32 = 25;
+const EGGNOG_INPUT: u32 = 150;
+
+fn parse(raw_input: &str) -> Vec<u32> {
+    let mut containers: Vec<u32> = vec![];
+    for line in raw_input.lines() {
+        containers.push(line.parse::<u32>().unwrap());
     }
-    yyy
+    containers
 }
 
-fn day17_part1(_example: &Vec<_>, _input: &Vec<_>) {
-    println!("TODO - part1");
+fn recursive_nb_combinations_containers(containers_left: &mut Vec<u32>, total_containers: u32, total_eggnog: u32) -> u32 {
+    if total_containers > total_eggnog {
+        return 0;
+    }
+    if containers_left.is_empty() {
+        if total_containers == total_eggnog {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    let container = containers_left.pop().unwrap();
+    let s = recursive_nb_combinations_containers(containers_left, total_containers + container, total_eggnog)
+        + recursive_nb_combinations_containers(containers_left, total_containers, total_eggnog);
+    containers_left.push(container);
+    s
+}
+
+fn day17_part1(example: &mut Vec<u32>, input: &mut Vec<u32>) {
+    println!("Example:  total: {EGGNOG_EXAMPLE:>3} | containers: {example:?}");
+    println!("Input:    total: {EGGNOG_INPUT:>3} | containers: {input:?}");
     // Exemple tests
-    // assert_eq!(, 0);
+    assert_eq!(recursive_nb_combinations_containers(example, 0, EGGNOG_EXAMPLE), 4);
 
     // Solve puzzle
-    // println!("Result part 1: {}");
-    // assert_eq!(, );
-    // println!("> DAY17 - part 1: OK!");
+    let res = recursive_nb_combinations_containers(input, 0, EGGNOG_INPUT);
+    println!("Result part 1: {res}");
+    assert_eq!(res, 4372);
+    println!("> DAY17 - part 1: OK!");
 }
 
-fn day17_part2(_example: &Vec<_>, _input: &Vec<_>) {
+fn day17_part2(_example: &mut Vec<u32>, _input: &mut Vec<u32>) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
