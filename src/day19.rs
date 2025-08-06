5d1f76752592f5cbb0e70f2621c31eee3543ad1f
Clémentine Scohy <clembytes@proton.me>
2025-08-06 16:39:26 +0200

    DAY19: part1 OK

--- src/day19.rs
+++ src/day19.rs
@@ -1,4 +1,7 @@
-use std::fs;
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 #[test]
 fn test() {
@@ -14,34 +17,72 @@ pub fn run() {
     day19_part2(&input);
 }
 
-fn parse(raw_input: &String) -> Vec<_> {
-    let mut yyy: Vec<_> = vec![];
-    for _line in raw_input.lines() {
-        // TODO
-        // yyy.push(line);
+fn parse(raw_input: &str) -> (HashMap<&str, Vec<&str>>, &str) {
+    let mut replacements: HashMap<&str, Vec<&str>> = HashMap::new();
+    let (list_of_replacements, molecule) = raw_input.split_once("\n\n").unwrap();
+    for line in list_of_replacements.lines() {
+        let (before, after) = line.split_once(" => ").unwrap();
+        let list_of_replacements = replacements.entry(before).or_default();
+        list_of_replacements.push(after);
     }
-    yyy
+    (replacements, molecule)
 }
 
-fn day19_part1(_input: &Vec<_>) {
-    println!("TODO - part1");
+fn find_nb_distinct_creations(input: &(HashMap<&str, Vec<&str>>, &str)) -> usize {
+    let mut creations: HashSet<String> = HashSet::new();
+    let (replacements, molecule) = input;
+    let n = molecule.len();
+    for (i, _) in molecule.char_indices() {
+        let before = &molecule[..i].to_string();
+        if replacements.contains_key(&molecule[i..i + 1]) {
+            let after = &molecule[i + 1..];
+            for &repl in replacements.get(&molecule[i..i + 1]).unwrap() {
+                let mut new_creation = before.to_owned() + repl;
+                new_creation += after;
+                creations.insert(new_creation);
+            }
+        } else if i < n - 1 && replacements.contains_key(&molecule[i..i + 2]) {
+            let after = &molecule[i + 2..];
+            for &repl in replacements.get(&molecule[i..i + 2]).unwrap() {
+                let mut new_creation = before.to_owned() + repl;
+                new_creation += after;
+                creations.insert(new_creation);
+            }
+        }
+    }
+    creations.len()
+}
+
+fn day19_part1(input: &(HashMap<&str, Vec<&str>>, &str)) {
     // Exemple tests
-    // assert_eq!(, 0);
+    let mut replacements_example: HashMap<&str, Vec<&str>> = HashMap::new();
+    replacements_example.insert("H", vec!["HO", "OH"]);
+    replacements_example.insert("O", vec!["HH"]);
+    let molecule_example = "HOH";
+    assert_eq!(
+        find_nb_distinct_creations(&(replacements_example.clone(), molecule_example)),
+        4
+    );
+    let molecule_example = "HOHOHO";
+    assert_eq!(
+        find_nb_distinct_creations(&(replacements_example, molecule_example)),
+        7
+    );
 
     // Solve puzzle
-    // let res = 
-    // println!("Result part 1: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY19 - part 1: OK!");
+    let res = find_nb_distinct_creations(input);
+    println!("Result part 1: {res}");
+    assert_eq!(res, 576);
+    println!("> DAY19 - part 1: OK!");
 }
 
-fn day19_part2(_input: &Vec<_>) {
+fn day19_part2(_input: &(HashMap<&str, Vec<&str>>, &str)) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
 
     // Solve puzzle
-    // let res = 
+    // let res =
     // println!("Result part 2: {res}");
     // assert_eq!(res, );
     // println!("> DAY19 - part 2: OK!");
