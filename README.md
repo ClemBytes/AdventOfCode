7012b2aa24d93457330ea09a272a7dfc84694fd9
Clémentine Scohy <clembytes@proton.me>
2025-08-07 09:31:21 +0200

    DAY20: part2 OK

--- README.md
+++ README.md
@@ -57,5 +57,5 @@ cargo test --release -- --nocapture dayXX
 I'm solving the problems at my own pace and may refactor as I go.  
 Progress so far:  
 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆  
-⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆☆  
-**39 / 50 stars**
+⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆  
+**40 / 50 stars**
--- src/day20.rs
+++ src/day20.rs
@@ -11,7 +11,7 @@ pub fn run() {
     day20_part2(input);
 }
 
-fn find_nb_presents_house_n_part1(house_number: u32) -> u32 {
+fn find_nb_presents_house_n(house_number: u32) -> u32 {
     let mut divisors: Vec<u32> = vec![];
     let sqrt_n = (house_number as f64).sqrt() as u32;
     for i in 1..=sqrt_n {
@@ -25,32 +25,10 @@ fn find_nb_presents_house_n_part1(house_number: u32) -> u32 {
     10 * divisors.iter().sum::<u32>()
 }
 
-fn find_nb_presents_house_n_part2(house_number: u32) -> u32 {
-    let mut divisors: Vec<u32> = vec![];
-    let sqrt_n = (house_number as f64).sqrt() as u32;
-    for i in 1..=sqrt_n {
-        if house_number % i == 0 {
-            divisors.push(i);
-            if i != house_number / i {
-                divisors.push(house_number / i);
-            }
-        }
-    }
-    divisors.sort();
-    11 * divisors[..50.min(divisors.len())].iter().sum::<u32>()
-}
-
-fn find_lowest_house_number(n: u32, part: u32) -> u32 {
+fn find_lowest_house_number(n: u32) -> u32 {
     let mut house_number = 1;
     loop {
-        let mut nb_presents = 0;
-        if part == 1 {
-            nb_presents = find_nb_presents_house_n_part1(house_number);
-        } else if part ==2 {
-            nb_presents = find_nb_presents_house_n_part2(house_number);
-        } else {
-            panic!("Part should 1 or 2 and not {part}!");
-        }
+        let nb_presents = find_nb_presents_house_n(house_number);
         if nb_presents >= n {
             return house_number;
         }
@@ -58,20 +36,41 @@ fn find_lowest_house_number(n: u32, part: u32) -> u32 {
     }
 }
 
+fn find_lowest_house_number_part2(n: u32) -> u32 {
+    let max_houses = 1_000_000;
+    let mut presents = vec![0u32; max_houses + 1];
+    // presents[k] contains the number of presents delivered in house k
+    for elf in 1..=max_houses {
+        for i in 1..=50 {
+            if i * elf <= max_houses {
+                presents[i * elf] += 11 * elf as u32;
+            } else {
+                break;
+            }
+        }
+    }
+    for (i, &nb_presents) in presents.iter().enumerate() {
+        if nb_presents >= n {
+            return i as u32;
+        }
+    }
+    panic!("House not found!");
+}
+
 fn day20_part1(input: u32) {
     // Exemple tests
-    assert_eq!(find_lowest_house_number(10, 1), 1);
-    assert_eq!(find_lowest_house_number(30, 1), 2);
-    assert_eq!(find_lowest_house_number(40, 1), 3);
-    assert_eq!(find_lowest_house_number(70, 1), 4);
-    assert_eq!(find_lowest_house_number(60, 1), 4);
-    assert_eq!(find_lowest_house_number(120, 1), 6);
-    assert_eq!(find_lowest_house_number(80, 1), 6);
-    assert_eq!(find_lowest_house_number(150, 1), 8);
-    assert_eq!(find_lowest_house_number(130, 1), 8);
+    assert_eq!(find_lowest_house_number(10), 1);
+    assert_eq!(find_lowest_house_number(30), 2);
+    assert_eq!(find_lowest_house_number(40), 3);
+    assert_eq!(find_lowest_house_number(70), 4);
+    assert_eq!(find_lowest_house_number(60), 4);
+    assert_eq!(find_lowest_house_number(120), 6);
+    assert_eq!(find_lowest_house_number(80), 6);
+    assert_eq!(find_lowest_house_number(150), 8);
+    assert_eq!(find_lowest_house_number(130), 8);
 
     // Solve puzzle
-    let res = find_lowest_house_number(input, 1);
+    let res = find_lowest_house_number(input);
     println!("Result part 1: {res}");
     assert_eq!(res, 665280);
     println!("> DAY20 - part 1: OK!");
@@ -79,8 +78,8 @@ fn day20_part1(input: u32) {
 
 fn day20_part2(input: u32) {
     // Solve puzzle
-    let res = find_lowest_house_number(input, 2);
+    let res = find_lowest_house_number_part2(input);
     println!("Result part 2: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY20 - part 2: OK!");
+    assert_eq!(res, 705600);
+    println!("> DAY20 - part 2: OK!");
 }
