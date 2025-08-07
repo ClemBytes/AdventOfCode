ce1ac8e5bd22c6d53842e4afb0a93322d3f2f448
Clémentine Scohy <clembytes@proton.me>
2025-08-07 16:36:54 +0200

    DAY22: preparing data

--- src/day22.rs
+++ src/day22.rs
@@ -7,43 +7,72 @@ fn test() {
 
 pub fn run() {
     println!("------- DAY22 -------");
-    let example = fs::read_to_string("inputs/example_day22").expect("Unable to read input!");
-    let example = parse(&example);
     let input = fs::read_to_string("inputs/input_day22").expect("Unable to read input!");
-    let input = parse(&input);
+    let boss = parse(&input);
 
-    day22_part1(&example, &input);
-    day22_part2(&example, &input);
+    let player = Player {
+        hit_points: 50,
+        mana: 500,
+        armor: 0,
+        shield_effect: 0,
+        recharge_effect: 0,
+    };
+
+    day22_part1(boss.clone(), player.clone());
+    day22_part2(boss, player);
 }
 
-fn parse(raw_input: &str) -> Vec<_> {
-    let mut yyy: Vec<_> = vec![];
-    for _line in raw_input.lines() {
-        // TODO
-        // yyy.push(line);
+#[derive(Clone, Debug)]
+struct Boss {
+    hit_points: i32,
+    damage: i32,
+    // Poison deals 3 damages to the boss every turn for 6 turns
+    poison_effect: i32, // Nb of turns where this effect will still be active
+}
+
+#[derive(Clone, Debug)]
+struct Player {
+    hit_points: i32,
+    mana: i32,
+    armor: i32,
+    // Shield increases player's armor by 7 during 6 turns
+    shield_effect: i32, // Nb of turns where this effect will still be active
+    // Recharge increases player's mana by 101 every turn for 5 turns
+    recharge_effect: i32, // Nb of turns where this effect will still be active
+}
+
+fn parse(raw_input: &str) -> Boss {
+    let (hit_points, damage) = raw_input.split_once("\n").unwrap();
+    Boss {
+        hit_points: hit_points.split(": ").collect::<Vec<&str>>()[1]
+            .parse::<i32>()
+            .unwrap(),
+        damage: damage.split(": ").collect::<Vec<&str>>()[1]
+            .parse::<i32>()
+            .unwrap(),
+        poison_effect: 0,
     }
-    yyy
 }
 
-fn day22_part1(_example: &Vec<_>, _input: &Vec<_>) {
+fn day22_part1(_boss: Boss, _player: Player) {
     println!("TODO - part1");
     // Exemple tests
     // assert_eq!(, 0);
 
     // Solve puzzle
-    // let res = 
+    // let res =
     // println!("Result part 1: {res}");
     // assert_eq!(res, );
     // println!("> DAY22 - part 1: OK!");
 }
 
-fn day22_part2(_example: &Vec<_>, _input: &Vec<_>) {
+fn day22_part2(_boss: Boss, _player: Player) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
 
     // Solve puzzle
-    // let res = 
+    // let res =
     // println!("Result part 2: {res}");
     // assert_eq!(res, );
     // println!("> DAY22 - part 2: OK!");
