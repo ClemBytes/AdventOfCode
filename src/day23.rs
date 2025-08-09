66d7eacb8adbe99d21d36a77381fc66908c6ad4a
Clémentine Scohy <clembytes@proton.me>
2025-08-09 19:34:06 +0200

    DAY23: refactor

--- src/day23.rs
+++ src/day23.rs
@@ -9,10 +9,10 @@ pub fn run() {
     println!("------- DAY23 -------");
 
     let example = vec![
-        Instructions::Increment(Register::A),
-        Instructions::JumpIfOne(Register::A, 2),
-        Instructions::Triple(Register::A),
-        Instructions::Increment(Register::A),
+        Instruction::Increment(Register::A),
+        Instruction::JumpIfOne(Register::A, 2),
+        Instruction::Triple(Register::A),
+        Instruction::Increment(Register::A),
     ];
 
     let input = fs::read_to_string("inputs/input_day23").expect("Unable to read input!");
@@ -22,75 +22,51 @@ pub fn run() {
     day23_part2(&input);
 }
 
-fn parse(raw_input: &str) -> Vec<Instructions> {
-    let mut instructions: Vec<Instructions> = vec![];
-    for line in raw_input.lines() {
-        let (command, arguments) = line.split_once(" ").unwrap();
-        match command {
-            "hlf" => {
-                let register = match arguments {
-                    "a" => Register::A,
-                    "b" => Register::B,
-                    other => {
-                        panic!("Unknown register: {other}");
-                    }
-                };
-                instructions.push(Instructions::Half(register));
-            }
-            "tpl" => {
-                let register = match arguments {
-                    "a" => Register::A,
-                    "b" => Register::B,
-                    other => {
-                        panic!("Unknown register: {other}");
-                    }
-                };
-                instructions.push(Instructions::Triple(register));
-            }
-            "inc" => {
-                let register = match arguments {
-                    "a" => Register::A,
-                    "b" => Register::B,
-                    other => {
-                        panic!("Unknown register: {other}");
-                    }
-                };
-                instructions.push(Instructions::Increment(register));
-            }
-            "jmp" => {
-                let offset: i32 = arguments.parse().unwrap();
-                instructions.push(Instructions::Jump(offset));
-            }
-            "jie" => {
-                let (register, offset) = arguments.split_once(", ").unwrap();
-                let register = match register {
-                    "a" => Register::A,
-                    "b" => Register::B,
-                    other => {
-                        panic!("Unknown register: {other}");
-                    }
-                };
-                let offset: i32 = offset.parse().unwrap();
-                instructions.push(Instructions::JumpIfEven(register, offset));
-            }
-            "jio" => {
-                let (register, offset) = arguments.split_once(", ").unwrap();
-                let register = match register {
-                    "a" => Register::A,
-                    "b" => Register::B,
-                    other => {
-                        panic!("Unknown register: {other}");
-                    }
-                };
-                let offset: i32 = offset.parse().unwrap();
-                instructions.push(Instructions::JumpIfOne(register, offset));
-            }
-            other => {
-                panic!("Unknwon command {other}");
-            }
-        }
+fn parse_argument(name: &str) -> Register {
+    match name {
+        "a" => Register::A,
+        "b" => Register::B,
+        other => panic!("Unknown register: {other}"),
     }
-    instructions
+}
+
+fn parse_instruction(line: &str) -> Instruction {
+    let (command, arguments) = line.split_once(" ").unwrap();
+    match command {
+        "hlf" => {
+            let register = parse_argument(arguments);
+            Instruction::Half(register)
+        }
+        "tpl" => {
+            let register = parse_argument(arguments);
+            Instruction::Triple(register)
+        }
+        "inc" => {
+            let register = parse_argument(arguments);
+            Instruction::Increment(register)
+        }
+        "jmp" => {
+            let offset: i32 = arguments.parse().unwrap();
+            Instruction::Jump(offset)
+        }
+        "jie" => {
+            let (register, offset) = arguments.split_once(", ").unwrap();
+            let register = parse_argument(register);
+            let offset: i32 = offset.parse().unwrap();
+            Instruction::JumpIfEven(register, offset)
+        }
+        "jio" => {
+            let (register, offset) = arguments.split_once(", ").unwrap();
+            let register = parse_argument(register);
+            let offset: i32 = offset.parse().unwrap();
+            Instruction::JumpIfOne(register, offset)
+        }
+        other => panic!("Unknwon command {other}"),
+    }
+}
+
+fn parse(raw_input: &str) -> Vec<Instruction> {
+    raw_input.lines().map(parse_instruction).collect()
 }
 
 #[derive(Debug)]
@@ -100,7 +76,7 @@ enum Register {
 }
 
 #[derive(Debug)]
-enum Instructions {
+enum Instruction {
     Half(Register),
     Triple(Register),
     Increment(Register),
@@ -109,49 +85,37 @@ enum Instructions {
     JumpIfOne(Register, i32),
 }
 
-fn program(instructions: &[Instructions], start_a: u32, start_b: u32) -> (u32, u32) {
+fn program(instructions: &[Instruction], start_a: u32, start_b: u32) -> (u32, u32) {
     let mut a = start_a;
     let mut b = start_b;
     let mut index: i32 = 0;
     while (index as usize) < instructions.len() && index >= 0 {
         match &instructions[index as usize] {
-            Instructions::Half(register) => {
+            Instruction::Half(register) => {
                 match register {
-                    Register::A => {
-                        a /= 2;
-                    }
-                    Register::B => {
-                        b /= 2;
-                    }
-                };
+                    Register::A => a /= 2,
+                    Register::B => b /= 2,
+                }
                 index += 1;
             }
-            Instructions::Triple(register) => {
+            Instruction::Triple(register) => {
                 match register {
-                    Register::A => {
-                        a *= 3;
-                    }
-                    Register::B => {
-                        b *= 3;
-                    }
-                };
+                    Register::A => a *= 3,
+                    Register::B => b *= 3,
+                }
                 index += 1;
             }
-            Instructions::Increment(register) => {
+            Instruction::Increment(register) => {
                 match register {
-                    Register::A => {
-                        a += 1;
-                    }
-                    Register::B => {
-                        b += 1;
-                    }
-                };
+                    Register::A => a += 1,
+                    Register::B => b += 1,
+                }
                 index += 1;
             }
-            Instructions::Jump(offset) => {
+            Instruction::Jump(offset) => {
                 index += offset;
             }
-            Instructions::JumpIfEven(register, offset) => {
+            Instruction::JumpIfEven(register, offset) => {
                 let considered_register_value = match register {
                     Register::A => a,
                     Register::B => b,
@@ -162,7 +126,7 @@ fn program(instructions: &[Instructions], start_a: u32, start_b: u32) -> (u32, u
                     index += 1;
                 }
             }
-            Instructions::JumpIfOne(register, offset) => {
+            Instruction::JumpIfOne(register, offset) => {
                 let considered_register_value = match register {
                     Register::A => a,
                     Register::B => b,
@@ -178,7 +142,7 @@ fn program(instructions: &[Instructions], start_a: u32, start_b: u32) -> (u32, u
     (a, b)
 }
 
-fn day23_part1(example: &[Instructions], input: &[Instructions]) {
+fn day23_part1(example: &[Instruction], input: &[Instruction]) {
     // Exemple tests
     let (a, b) = program(example, 0, 0);
     println!("Result example part 1: a = {a} | b = {b}");
@@ -191,7 +155,7 @@ fn day23_part1(example: &[Instructions], input: &[Instructions]) {
     println!("> DAY23 - part 1: OK!");
 }
 
-fn day23_part2(input: &[Instructions]) {
+fn day23_part2(input: &[Instruction]) {
     // Solve puzzle
     let (a, b) = program(input, 1, 0);
     println!("Result part 1: a = {a} | b = {b}");
