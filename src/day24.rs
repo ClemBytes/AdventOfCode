5d2061768ef53292b150720fa74ecbf835814c51
Clémentine Scohy <clembytes@proton.me>
2025-08-10 18:26:06 +0200

    24: optim (delete unused code)

--- src/day24.rs
+++ src/day24.rs
@@ -21,16 +21,6 @@ pub fn run() {
     day24_part2(&example, &input);
 }
 
-// #[test]
-// fn testlkjlkja() {
-//     let bla = can_be_balanced_in_3_groups(&[1, 3, 5, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 107, 101, 97, 89, 83, 79, 73, 71, 67]);
-//     println!("{bla}");
-// // }
-
-// fn nb_packages_group1(state: Vec<u8>) -> u8 {
-//     state.iter().sum()
-// }
-
 fn quantum_entanglement(state: &[u64], list_of_packages: &[u64]) -> u64 {
     let mut qe = 1;
     for (i, is_in_group1) in state.iter().enumerate() {
@@ -42,7 +32,7 @@ fn quantum_entanglement(state: &[u64], list_of_packages: &[u64]) -> u64 {
 }
 
 fn can_be_balanced(list_of_packages: &[u64], used: &mut Vec<u64>, target_weight: u64, used_weight: u64, nb_groups: usize) -> bool {
-    // Only one group or no packages to balance: trivially OK
+    // Only one group: trivially OK
     if nb_groups == 1 {
         return true;
     }
@@ -69,35 +59,6 @@ fn can_be_balanced(list_of_packages: &[u64], used: &mut Vec<u64>, target_weight:
     }
 }
 
-/*
-#[test]
-fn test_find_subset() {
-    // Test possible subset
-    assert!(find_subset(&[1, 5, 2, 6, 3, 4, 8], &mut vec![0, 1, 0, 1, 0, 0, 1], 5));
-    assert!(find_subset(&[1, 5, 2, 6, 3, 4, 8], &mut vec![0, 1, 0, 1, 0, 1, 1], 3));
-    assert!(find_subset(&[1, 2, 3, 3, 4, 5, 9, 12], &mut vec![0, 1, 0, 0, 1, 0, 1, 1], 6));
-    // Test impossible subset
-    assert!(!find_subset(&[3, 7], &mut vec![0, 0], 5));
-    assert!(!find_subset(&[3, 3, 4], &mut vec![0, 0, 0], 5));
-    assert!(!find_subset(&[1, 2, 3, 3, 4, 5, 9, 12], &mut vec![1, 0, 1, 1, 1, 1, 1, 0], 7));
-}
-*/
-
-#[test]
-fn test_can_be_balanced() {
-    // Balance 3 groups
-    /*
-    assert!(!can_be_balanced(&[1, 2, 3, 4, 5], &mut vec![0, 0, 0, 0, 0], 5, 3)); // [] is used, can't do 0 whith the others
-    assert!(can_be_balanced(&[1, 2, 3, 4, 5], &mut vec![0, 0, 0, 0, 1], 5, 3)); // [5] is used, can do [1, 4] = 5 and [2, 3] = 5
-    assert!(can_be_balanced(&[5, 5, 5], &mut vec![0, 0, 1], 5, 3)); // [5] is used, can do [5] = 5 and [5] = 5
-    assert!(!can_be_balanced(&[1, 2, 3], &mut vec![1, 0, 0], 3)); // [1] is used and no possibility to do 1 with 2 and 3
-    assert!(can_be_balanced(&[2, 4, 2, 1, 3], &mut vec![0, 1, 0, 0, 0], 3)); // [4] is used and can do [2, 2] = 4 and [1, 3] = 4
-    assert!(!can_be_balanced(&[1, 2, 4, 2, 1, 3], &mut vec![1, 0, 1, 0, 0, 0], 3)); // [1, 4] = 5 is used and can do [1, 2, 2] = 5 but then [3] != 5
-    assert!(!can_be_balanced(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1], &mut vec![0, 1, 0, 1, 0, 1, 0, 1, 0, 1], 3)); // [1, 1, 1, 1, 1] = 5 is used and then we can do [1, 1, 1, 1, 1] = 5 but then [] != 5
-    assert!(can_be_balanced(&[1, 1, 1, 1, 1, 1, 1, 1, 1], &mut vec![0, 1, 0, 0, 0, 0, 0, 1, 1], 3)); // [1, 1, 1] = 3 is used; can do twice [1, 1, 1] = 3
-    */
-}
-
 fn find_ideal_configuration(list_of_packages: &[u64], nb_groups: usize) -> u64 {
     let total_weight: u64 = list_of_packages.iter().sum();
     assert_eq!(total_weight % (nb_groups as u64), 0);
@@ -112,7 +73,7 @@ fn find_ideal_configuration(list_of_packages: &[u64], nb_groups: usize) -> u64 {
             return quantum_entanglement(&used, list_of_packages);
         }
 
-        // Else: add neighbors to heap
+        // Else: add neighbors to queue
         // Find last package added
         let mut next_package_index = 0;
         for (i, is_used) in used.iter().enumerate() {
@@ -139,7 +100,7 @@ fn day24_part1(example: &[u64], input: &[u64]) {
     // Solve puzzle
     let res = find_ideal_configuration(input, 3);
     println!("Result part 1: {res}");
-    assert_eq!(res, 11266889531); // 22554787 is too low, 326440069283 is too high
+    assert_eq!(res, 11266889531);
     println!("> DAY24 - part 1: OK!");
 }
 
