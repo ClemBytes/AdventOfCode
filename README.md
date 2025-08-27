c0d00a5b64a88611a6e2c9d044a12b4b6da0d373
Clémentine Scohy <clembytes@proton.me>
2025-08-27 11:34:28 +0200

    DAY13: part2 OK

--- README.md
+++ README.md
@@ -57,5 +57,5 @@ I'm solving the problems at my own pace and may refactor as I go.
 
 Progress so far:  
 ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆☆☆☆☆☆☆☆   
-⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆☆☆☆☆☆☆☆☆   
-**25 / 50 stars**
+⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐☆☆☆☆☆☆☆☆☆☆☆☆   
+**26 / 50 stars**
--- src/day13.rs
+++ src/day13.rs
@@ -11,7 +11,7 @@ pub fn run() {
     let input = 1350;
 
     day13_part1(example, input);
-    day13_part2(example, input);
+    day13_part2(input);
 }
 
 #[derive(Debug, PartialEq)]
@@ -60,6 +60,35 @@ fn reach(start: (i32, i32), end: (i32, i32), designer_fav: i32) -> i32 {
     unreachable!()
 }
 
+fn get_nb_locations(start: (i32, i32), nb_steps: i32, designer_fav: i32) -> usize {
+    let mut q = VecDeque::new();
+    q.push_back((0, start));
+    let mut visited = HashSet::new();
+    while let Some((nb_steps_current, current)) = q.pop_front() {
+        if visited.contains(&current) {
+            continue;
+        }
+        visited.insert(current);
+
+        if nb_steps_current == nb_steps {
+            return visited.len();
+        }
+
+        for (x, y) in [
+            (current.0 - 1, current.1),
+            (current.0 + 1, current.1),
+            (current.0, current.1 - 1),
+            (current.0, current.1 + 1),
+        ] {
+            if x >= 0 && y >= 0 && get_location_kind(x, y, designer_fav) == LocationType::OpenSpace
+            {
+                q.push_back((nb_steps_current + 1, (x, y)));
+            }
+        }
+    }
+    unreachable!()
+}
+
 fn day13_part1(example: i32, input: i32) {
     // Exemple tests
     let res = reach((1, 1), (7, 4), example);
@@ -72,14 +101,10 @@ fn day13_part1(example: i32, input: i32) {
     println!("> DAY13 - part 1: OK!");
 }
 
-fn day13_part2(_example: i32, _input: i32) {
-    println!("TODO - part2");
-    // Exemple tests
-    // assert_eq!(, 0);
-
+fn day13_part2(input: i32) {
     // Solve puzzle
-    // let res =
-    // println!("Result part 2: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY13 - part 2: OK!");
+    let res = get_nb_locations((1, 1), 50, input);
+    println!("Result part 2: {res}");
+    assert_eq!(res, 124);
+    println!("> DAY13 - part 2: OK!");
 }
