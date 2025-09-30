d8523653afe8cf34c57429baafa77c59f6e919d5
Clémentine Scohy <clembytes@proton.me>
2025-09-30 22:45:14 +0200

    2016: DAY25: OK!!!

--- 2016/src/day25.rs
+++ 2016/src/day25.rs
@@ -1,230 +1,77 @@
-use std::fs;
-
 #[test]
 fn test() {
     run();
 }
 
 pub fn run() {
-    println!("------- DAY23 -------");
-    let input = fs::read_to_string("inputs/input_day25").expect("Unable to read input!");
-    let input = parse(&input);
-
-    day25(&input);
+    println!("------- DAY25 -------");
+    day25();
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Register {
-    A,
-    B,
-    C,
-    D,
-}
-
-impl Register {
-    fn from_str(s: &str) -> Option<Self> {
-        match s {
-            "a" => Some(Register::A),
-            "b" => Some(Register::B),
-            "c" => Some(Register::C),
-            "d" => Some(Register::D),
-            _ => None,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Instruction {
-    Cpyr(Register, Register),
-    Cpyi(i32, Register),
-    Inc(Register),
-    Dec(Register),
-    Jnzri(Register, i32),
-    Jnzir(i32, Register),
-    Out(Register),
-}
-
-fn parse(raw_input: &str) -> Vec<Instruction> {
-    let mut instructions = vec![];
-    for line in raw_input.lines() {
-        let (instruction_kind, infos) = line.split_once(" ").unwrap();
-        match instruction_kind {
-            "cpy" => {
-                let (x, y) = infos.split_once(" ").unwrap();
-                let y = Register::from_str(y).unwrap();
-                if let Some(x) = Register::from_str(x) {
-                    instructions.push(Instruction::Cpyr(x, y));
-                } else {
-                    let x = x.parse().unwrap();
-                    instructions.push(Instruction::Cpyi(x, y));
-                }
-            }
-            "inc" => {
-                let x = Register::from_str(infos).unwrap();
-                instructions.push(Instruction::Inc(x));
-            }
-            "dec" => {
-                let x = Register::from_str(infos).unwrap();
-                instructions.push(Instruction::Dec(x));
-            }
-            "jnz" => {
-                let (x, y) = infos.split_once(" ").unwrap();
-                if let Some(x) = Register::from_str(x) {
-                    let y = y.parse().unwrap();
-                    instructions.push(Instruction::Jnzri(x, y));
-                } else {
-                    let x = x.parse().unwrap();
-                    let y = Register::from_str(y).unwrap();
-                    instructions.push(Instruction::Jnzir(x, y));
-                }
-            }
-            "out" => {
-                let x = Register::from_str(infos).unwrap();
-                instructions.push(Instruction::Out(x));
-            }
-            _ => panic!("Unknown instruction: {instruction_kind} ({infos})!"),
-        }
-    }
-    instructions
-}
-
-fn execute_code(instructions: &[Instruction], init_a: i32) -> i32 {
-    let mut instructions: Vec<Instruction> = instructions.to_vec();
-    // Initialize
+/*
+fn execute_code(init_a: i32){
     let mut a = init_a;
-    let mut b = 0;
-    let mut c = 0;
-    let mut d = 0;
-    let mut i = 0;
-    let nb_instructions = instructions.len();
+    let mut d = a;
+    let mut c = 4;
+    let mut b = 633;
+    // inc d
+    // dec b
+    // jnz b -2 <=> (d += 1) × 633 <=> d += 633;
+    // dec c
+    // jnz c -5 (back to b = 633) <=> (d += 633) × 4 <=> d += 633*4
+    d += 633 * 4; // Here, we have d = init_a + 633 * 4;
+    a = d; // So a = init_a + 633 * 4;
+    // jnz 0 0 is useless
+    b = a; // So b = init_a + 633 * 4;
+    // a = 0;
+    // while b > 0 {
+    //     // c = 2;
+    //     // while c != 0 {
+    //     //     b -= 1;
+    //     //     c -= 1;
+    //     // } // <=> b -= 2;
+    //     // a += 1;
+    //     b -= 2;
+    //     a += 1;
+    // } // <=> a = b % 2; and b = b % 2;
+    a = b / 2; // <=> a = (init_a + 633 * 4) / 2;
+    b = b % 2; // <=> b = (init_a + 633 * 4) % 2;
+    while c != 0 {
+        b -= 1;
+        c -= 1;
+    } // <=> b -= c;
+    println!("{b}");
+}
+*/
 
-    while i < nb_instructions {
-        // println!("i: {i} | a: {a} | b: {b} | c: {c} | d: {d}");
-        match instructions[i] {
-            Instruction::Cpyr(rx, ry) => {
-                let val_to_copy = match rx {
-                    Register::A => a,
-                    Register::B => b,
-                    Register::C => c,
-                    Register::D => d,
-                };
-                match ry {
-                    Register::A => {
-                        a = val_to_copy;
-                    }
-                    Register::B => {
-                        b = val_to_copy;
-                    }
-                    Register::C => {
-                        c = val_to_copy;
-                    }
-                    Register::D => {
-                        d = val_to_copy;
-                    }
-                };
-                i += 1;
-            }
-            Instruction::Cpyi(val, r) => {
-                match r {
-                    Register::A => {
-                        a = val;
-                    }
-                    Register::B => {
-                        b = val;
-                    }
-                    Register::C => {
-                        c = val;
-                    }
-                    Register::D => {
-                        d = val;
-                    }
-                };
-                i += 1;
-            }
-            Instruction::Inc(r) => {
-                match r {
-                    Register::A => {
-                        a += 1;
-                    }
-                    Register::B => {
-                        b += 1;
-                    }
-                    Register::C => {
-                        c += 1;
-                    }
-                    Register::D => {
-                        d += 1;
-                    }
-                };
-                i += 1;
-            }
-            Instruction::Dec(r) => {
-                match r {
-                    Register::A => {
-                        a -= 1;
-                    }
-                    Register::B => {
-                        b -= 1;
-                    }
-                    Register::C => {
-                        c -= 1;
-                    }
-                    Register::D => {
-                        d -= 1;
-                    }
-                };
-                i += 1;
-            }
-            Instruction::Jnzri(r_test, skip) => {
-                let val_test = match r_test {
-                    Register::A => a,
-                    Register::B => b,
-                    Register::C => c,
-                    Register::D => d,
-                };
-                if val_test != 0 {
-                    let mut new_index = i as i32;
-                    new_index += skip;
-                    if new_index >= 0 {
-                        i = new_index as usize;
-                    }
-                } else {
-                    i += 1;
-                }
-            }
-            Instruction::Jnzir(val_test, skip) => {
-                let skip = match skip {
-                    Register::A => a,
-                    Register::B => b,
-                    Register::C => c,
-                    Register::D => d,
-                };
-                if val_test != 0 {
-                    let mut new_index = i as i32;
-                    new_index += skip;
-                    if new_index >= 0 {
-                        i = new_index as usize;
-                    }
-                } else {
-                    i += 1;
-                }
-            }
-            Instruction::Out(clock_signal) => {
-                
-            }
+/*
+fn execute_code(init_a: i32) -> String {
+    let d = init_a + 633 * 4;
+    let mut a = init_a;
+    let mut s = String::from(&format!("{init_a}: ").to_string());
+    loop {
+        a = d;
+        while a != 0 {
+            let b = a % 2;
+            a = a / 2;
+            s += &format!("{b}").to_string();
         }
     }
-    a
 }
+So in fact this function prints the binary representation of (init_a + 2532) over and over
+So we are looking for the first number >= 2532 such that it's binary representation is '1010...10'.
+And 2532 in binary is '100111100100', so we need at least '10'×6
+*/
 
-// fn toggle_instruction
-
-fn day25(input: &[Instruction]) {
+fn day25() {
     // Solve puzzle
     let mut init_a = 0;
-    
-    let res = execute_code(input, 7);
-    println!("Result: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY25 OK!!!");
+    let mut x = 2532;
+    while !format!("{x:b}").starts_with("101010101010") {
+        init_a += 1;
+        x = 2532 + init_a;
+    }
+    println!("Result: {init_a}");
+    assert_eq!(init_a, 198);
+    println!("> DAY25 OK!!!");
 }
