6566480ab558614c3c03271070b28558962b1cbb
Clémentine Scohy <clembytes@proton.me>
2025-11-05 22:44:28 +0100

    2017: DAY15: part1 OK

--- 2017/src/day15.rs
+++ 2017/src/day15.rs
@@ -17,7 +17,11 @@ pub fn run() {
     day15_part2(example, input);
 }
 
-fn parse(raw_input: &str) -> (u32, u32) {
+const FACTOR_A: u64 = 16807;
+const FACTOR_B: u64 = 48271;
+const DIVIDER: u64 = 2147483647;
+
+fn parse(raw_input: &str) -> (u64, u64) {
     let mut a = 0;
     let mut b = 0;
     let r = Regex::new(r"^Generator (A|B) starts with ([0-9]+)$").unwrap();
@@ -26,35 +30,59 @@ fn parse(raw_input: &str) -> (u32, u32) {
         let name = &matches[1];
         let val = matches[2].parse().unwrap();
         match name {
-            "A" => {a = val},
-            "B" => {b = val},
-            other => {unreachable!("Unknown generator name: {other}")},
+            "A" => a = val,
+            "B" => b = val,
+            other => {
+                unreachable!("Unknown generator name: {other}")
+            }
         }
     }
     (a, b)
 }
 
-fn day15_part1(_example: (u32, u32), _input: (u32, u32)) {
-    println!("TODO - part1");
-    // Exemple tests
-    // assert_eq!(, 0);
-    // println!("Example OK");
-
-    // Solve puzzle
-    // let res = 
-    // println!("Result part 1: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY15 - part 1: OK!");
+fn judge_final_count(nb_pairs: u64, input: (u64, u64)) -> u64 {
+    let mut count = 0;
+    let (mut a, mut b) = input;
+    for _ in 1..=nb_pairs {
+        a = (a * FACTOR_A) % DIVIDER;
+        b = (b * FACTOR_B) % DIVIDER;
+        if (a & 0xFFFF) == (b & 0xFFFF) {
+            count += 1;
+        }
+    }
+    count
 }
 
-fn day15_part2(_example: (u32, u32), _input: (u32, u32)) {
+fn day15_part1(example: (u64, u64), input: (u64, u64)) {
+    // let a = example.0;
+    // let a_3 = (((((a * FACTOR_A) % DIVIDER) * FACTOR_A) % DIVIDER) * FACTOR_A) % DIVIDER;
+    // let a_3_16 = (a_3)&0xFFFF;
+    // println!("example A: {} | {} | {} | {:b} ", a, a_3, a_3_16, a_3_16);
+    // let b = example.1;
+    // let b_3 = (((((b * FACTOR_B) % DIVIDER) * FACTOR_B) % DIVIDER) * FACTOR_B) % DIVIDER;
+    // let b_3_16 = (b_3)&0xFFFF;
+    // println!("example B: {} | {} | {} | {:b} ", b, b_3, b_3_16, b_3_16);
+    // println!("input: {input:?}");
+
+    // Exemple tests
+    assert_eq!(judge_final_count(5, example), 1);
+    assert_eq!(judge_final_count(40_000_000, example), 588);
+
+    // Solve puzzle
+    let res = judge_final_count(40_000_000, input);
+    println!("Result part 1: {res}");
+    assert_eq!(res, 573);
+    println!("> DAY15 - part 1: OK!");
+}
+
+fn day15_part2(_example: (u64, u64), _input: (u64, u64)) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
     // println!("Example OK");
 
     // Solve puzzle
-    // let res = 
+    // let res =
     // println!("Result part 2: {res}");
     // assert_eq!(res, );
     // println!("> DAY15 - part 2: OK!");
