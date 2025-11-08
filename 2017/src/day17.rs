6ab7cfafa6ed1919533bb4fb0b25f3e5a5de89a9
Clémentine Scohy <clembytes@proton.me>
2025-11-08 12:06:41 +0100

    2017: DAY17: part2 naive

--- 2017/src/day17.rs
+++ 2017/src/day17.rs
@@ -11,7 +11,7 @@ pub fn run() {
     day17_part2(input);
 }
 
-fn complete_circuler_buffer(steps: usize, last_value: usize) -> usize {
+fn complete_circuler_buffer(steps: usize, last_value: usize, value_after: usize) -> usize {
     let mut buffer: Vec<usize> = vec![0];
     let mut current_position = 0;
     for i in 1..=last_value {
@@ -20,29 +20,25 @@ fn complete_circuler_buffer(steps: usize, last_value: usize) -> usize {
         current_position = new_position + 1;
         // println!("{:?} | current_position: {current_position} | current_position value: {}", buffer, buffer[current_position]);
     }
-    buffer[current_position + 1]
+    let next_value_position = buffer.iter().position(|&r| r == value_after).unwrap();
+    buffer[next_value_position + 1]
 }
 
 fn day17_part1(input: usize) {
     // Exemple tests
-    assert_eq!(complete_circuler_buffer(3, 2017), 638);
+    assert_eq!(complete_circuler_buffer(3, 2017, 2017), 638);
 
     // Solve puzzle
-    let res = complete_circuler_buffer(input, 2017);
+    let res = complete_circuler_buffer(input, 2017, 2017);
     println!("Result part 1: {res}");
     assert_eq!(res, 996);
     println!("> DAY17 - part 1: OK!");
 }
 
-fn day17_part2(_input: usize) {
-    println!("TODO - part2");
-    // Exemple tests
-    // assert_eq!(, 0);
-    // println!("Example OK");
-
+fn day17_part2(input: usize) {
     // Solve puzzle
-    // let res =
-    // println!("Result part 2: {res}");
+    let res = complete_circuler_buffer(input, 50_000_000, 0);
+    println!("Result part 2: {res}");
     // assert_eq!(res, );
     // println!("> DAY17 - part 2: OK!");
 }
