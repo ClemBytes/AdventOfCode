13cc20960ee9105a7578d0465945400201ee685d
Clémentine Scohy <clembytes@proton.me>
2025-11-26 21:08:51 +0100

    2017: DAY21: other representation

--- 2017/src/day21.rs
+++ 2017/src/day21.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 #[test]
 fn test() {
@@ -8,79 +8,12 @@ fn test() {
 pub fn run() {
     println!("------- DAY21 -------");
     let example = fs::read_to_string("inputs/example_day21").expect("Unable to read input!");
-    let example = Rule::parse(&example);
+    let example = parse(&example);
     let input = fs::read_to_string("inputs/input_day21").expect("Unable to read input!");
-    let input = Rule::parse(&input);
+    let input = parse(&input);
 
     day21_part1(&example, &input);
     day21_part2(&example, &input);
-}
-
-#[derive(Debug, Clone, Copy)]
-enum State {
-    On,
-    Off,
-}
-
-#[derive(Debug, Clone)]
-struct Rule {
-    size_in: usize,
-    input_pattern: Vec<Vec<State>>,
-    output_pattern: Vec<Vec<State>>,
-}
-
-impl Rule {
-    fn size_out(&self) -> usize {
-        match self.size_in {
-            2 => 3,
-            3 => 4,
-            other => unreachable!("Impossible size_in for Rule: {other}! Should be 2 or 3!"),
-        }
-    }
-
-    fn parse(raw_input: &str) -> Vec<Rule> {
-        let mut rules = vec![];
-        for line in raw_input.lines() {
-            let line = line.trim();
-            let (i, o) = line.split_once(" => ").unwrap();
-            let i_lines: Vec<&str> = i.split("/").collect();
-            let size_in = i_lines.len();
-            if size_in != 2 && size_in != 3 {
-                unreachable!("Impossible size_in {size_in} for Rule {line}! Should be 2 or 3!");
-            }
-            let mut input_pattern = vec![];
-            for i_line in i_lines {
-                let mut l = vec![];
-                for c in i_line.chars() {
-                    let s = match c {
-                        '#' => State::On,
-                        '.' => State::Off,
-                        other => unreachable!("Unknown state: '{other}' in line {line}! Should be '#' or '.''!"),
-                    };
-                    l.push(s);
-                }
-                input_pattern.push(l);
-            }
-
-            let o_lines: Vec<&str> = o.split("/").collect();
-            let mut output_pattern = vec![];
-            for o_line in o_lines {
-                let mut l = vec![];
-                for c in o_line.chars() {
-                    let s = match c {
-                        '#' => State::On,
-                        '.' => State::Off,
-                        other => unreachable!("Unknown state: '{other}' in line {line}! Should be '#' or '.''!"),
-                    };
-                    l.push(s);
-                }
-                output_pattern.push(l);
-            }
-
-            rules.push(Rule {size_in, input_pattern, output_pattern});
-        }
-        rules
-    }
 }
 
 // Idea: represent patterns with powers of 2:
@@ -88,10 +21,62 @@ impl Rule {
 // #./.# = 1×2⁰ + 0×2¹ + 0×2² + 1×2³ = 1 + 0 + 0 + 8 = 9 = 1001b
 // ##/## = 1×2⁰ + 1×2¹ + 1×2² + 1×2³ = 1 + 2 + 4 + 8 = 15 = 1111b
 // Well, it's just a binary representation
+// The example rules become:
+// ../.# => ##./#../...                 : 8 => 11
+// .#./..#/### => #..#/..../..../#..#   : 482 => 36_873
+// And if we add sizes:
+// ../.# => ##./#../...                 : (2, 8) => (3, 11)
+// .#./..#/### => #..#/..../..../#..#   : (3, 482) => (4, 36_873)
 
-fn day21_part1(_example: &Vec<Rule>, _input: &Vec<Rule>) {
-    let a = 9;
-    println!("{a:b}");
+fn parse(raw_input: &str) -> HashMap<(usize, usize), (usize, usize)> {
+    let mut rules = HashMap::new();
+    for line in raw_input.lines() {
+        let (i, o) = line.split_once(" => ").unwrap();
+        let mut input_pattern = 0;
+        let mut counter = 0;
+        for c in i.chars() {
+            match c {
+                '#' => {
+                    input_pattern += 2_usize.pow(counter);
+                    counter += 1;
+                },
+                '.' => {counter += 1;},
+                '/' => {},
+                other => unreachable!("Unknown state: '{other}' in line {line}! Should be '#' or '.''!"),
+            };
+        }
+        let size_in = match counter {
+            4 => 2,
+            9 => 3,
+            other => unreachable!("Impossible size_in {other} for Rule {line}! Should be 4 or 9!"),
+        };
+
+        let mut output_pattern = 0;
+        let mut counter = 0;
+        for c in o.chars() {
+            match c {
+                '#' => {
+                    output_pattern += 2_usize.pow(counter);
+                    counter += 1;
+                },
+                '.' => {counter += 1;},
+                '/' => {},
+                other => unreachable!("Unknown state: '{other}' in line {line}! Should be '#' or '.''!"),
+            };
+        }
+        let size_out = match counter {
+            9 => 3,
+            16 => 4,
+            other => unreachable!("Impossible size_out {other} for Rule {line}! Should be 9 or 16!"),
+        };
+
+        rules.insert((size_in, input_pattern), (size_out, output_pattern));
+    }
+    rules
+}
+
+fn day21_part1(_example: &HashMap<(usize, usize), (usize, usize)>, input: &HashMap<(usize, usize), (usize, usize)>) {
+    println!("{input:?}");
     // Exemple tests
     // assert_eq!(, 0);
     // println!("Example OK");
@@ -103,7 +88,7 @@ fn day21_part1(_example: &Vec<Rule>, _input: &Vec<Rule>) {
     // println!("> DAY21 - part 1: OK!");
 }
 
-fn day21_part2(_example: &Vec<Rule>, _input: &Vec<Rule>) {
+fn day21_part2(_example: &HashMap<(usize, usize), (usize, usize)>, _input: &HashMap<(usize, usize), (usize, usize)>) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
