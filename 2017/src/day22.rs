3e4bc0e109832d127117c137be7dd4aa74592a43
Clémentine Scohy <clembytes@proton.me>
2025-11-29 22:46:01 +0100

    2017: DAY22: part1 OK

--- 2017/src/day22.rs
+++ 2017/src/day22.rs
@@ -12,7 +12,7 @@ pub fn run() {
     let example_start_position = (1, 1);
     let input = fs::read_to_string("inputs/input_day22").expect("Unable to read input!");
     let input = parse(&input);
-    let input_start_position = (13, 13);
+    let input_start_position = (12, 12);
 
     day22_part1(
         &example,
@@ -28,10 +28,19 @@ pub fn run() {
     );
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Node {
     Clean,
     Infected,
+}
+
+impl Node {
+    fn revert(&self) -> Self {
+        match self {
+            Node::Clean => Node::Infected,
+            Node::Infected => Node::Clean,
+        }
+    }
 }
 
 fn parse(raw_input: &str) -> HashMap<(i32, i32), Node> {
@@ -62,9 +71,27 @@ impl Direction {
         let (ci, cj) = current_position;
         match self {
             Direction::Up => (ci - 1, cj),
-            Direction::Right => (ci, cj - 1),
+            Direction::Right => (ci, cj + 1),
             Direction::Down => (ci + 1, cj),
-            Direction::Left => (ci, cj + 1),
+            Direction::Left => (ci, cj - 1),
+        }
+    }
+
+    fn turn_left(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Right => Direction::Up,
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+        }
+    }
+
+    fn turn_right(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
         }
     }
 }
@@ -76,9 +103,27 @@ fn solve_part1(
 ) -> i32 {
     let mut current_position = start_position;
     let mut current_direction = Direction::Up;
+    let mut grid = start_grid.clone();
     let mut nb_new_infections = 0;
     for _ in 0..nb_bursts {
-        
+        grid.entry(current_position).or_insert(Node::Clean);
+
+        // Turn left or right
+        let n = grid.get(&current_position).unwrap();
+        current_direction = match n {
+            Node::Clean => current_direction.turn_left(),
+            Node::Infected => current_direction.turn_right(),
+        };
+
+        // Infect or clean
+        let n = n.revert();
+        if matches!(n, Node::Infected) {
+            nb_new_infections += 1;
+        }
+        grid.insert(current_position, n);
+
+        // Move
+        current_position = current_direction.next_position(current_position);
     }
     nb_new_infections
 }
@@ -93,13 +138,12 @@ fn day22_part1(
     assert_eq!(solve_part1(7, example_start_position, example), 5);
     assert_eq!(solve_part1(70, example_start_position, example), 41);
     assert_eq!(solve_part1(10_000, example_start_position, example), 5_587);
-    println!("Examples OK");
 
     // Solve puzzle
     let res = solve_part1(10_000, input_start_position, input);
     println!("Result part 1: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY22 - part 1: OK!");
+    assert_eq!(res, 5266);
+    println!("> DAY22 - part 1: OK!");
 }
 
 fn day22_part2(
