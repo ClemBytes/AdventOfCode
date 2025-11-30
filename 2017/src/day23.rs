ad3e543d1e190721eaa0a86ca87516ea2a20bea1
Clémentine Scohy <clembytes@proton.me>
2025-11-30 21:54:33 +0100

    2017: DAY23: part2: notes

--- 2017/src/day23.rs
+++ 2017/src/day23.rs
@@ -135,12 +135,43 @@ fn day23_part1(input: &[Instruction]) {
     println!("> DAY23 - part 1: OK!");
 }
 
-fn day23_part2(_input: &[Instruction]) {
-    println!("TODO - part2");
-    // Exemple tests
-    // assert_eq!(, 0);
-    // println!("Example OK");
+// Let's optimize:
+//
+// set b 79        | b = 79
+// set c b         | c = b
+// jnz a 2         | if a != 0:
+// jnz 1 5         |    |
+// mul b 100       |    b *= 100
+// sub b -100000   |    b += 100_000             b = 107_900
+// set c b         |    c = b
+// sub c -17000    |    c += 17_000              c = 124_900
 
+// set f 1         |    f = 1  <--------------------------------------------|
+// set d 2         |    d = 2                                               |
+// set e 2         |    e = 2  <---------------------------|                |
+// set g d         |    g = d  <--------|                  |                |
+// mul g e         |    g *= e          |                  |                |
+// sub g b         |    g -= b          |                  |                |
+// jnz g 2         |    if d*e-b != 0:  | else:            |                |
+// set f 0         |        |           |  f = 0           |                |
+// sub e -1        |        e += 1      |                  |                |
+// set g e         |        g = e       |                  |                |
+// sub g b         |        g -= b      |                  |                |
+// jnz g -8        |        if g != 0: -| else (g == 0):   |                |
+// sub d -1        |            d += 1                     |                |
+// set g d         |            g = d                      |                |
+// sub g b         |            g -= b                     |                |
+// jnz g -13       |            if g != 0: ----------------| else (g == 0): |
+// jnz f 2         |                if f != 0:      else:                   |
+// sub h -1        |                    |              h += 1               |
+// set g b         |                    g = b                               |
+// sub g c         |                    g -= c                              |
+// jnz g 2         |                    if g != 0: (else: finished)         |
+// jnz 1 3         |                        |                               |
+// sub b -17       |                        b += 17                         |
+// jnz 1 -23       |                        ---------------------------------
+
+fn day23_part2(_input: &[Instruction]) {
     // Solve puzzle
     // let res =
     // println!("Result part 2: {res}");
