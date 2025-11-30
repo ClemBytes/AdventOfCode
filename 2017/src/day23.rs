715adc4e9bd60959ad487adb9d70f78a696dd9cd
Clémentine Scohy <clembytes@proton.me>
2025-11-30 20:46:55 +0100

    2017: DAY23: part1 OK

---
+++ 2017/src/day23.rs
@@ -0,0 +1,149 @@
+use std::{collections::HashMap, fs};
+
+#[test]
+fn test() {
+    run();
+}
+
+pub fn run() {
+    println!("------- DAY23 -------");
+    let input = fs::read_to_string("inputs/input_day23").expect("Unable to read input!");
+    let input = Instruction::parse(&input);
+
+    day23_part1(&input);
+    day23_part2(&input);
+}
+
+#[derive(Debug, Clone)]
+enum RegisterOrValue {
+    Register(char),
+    Value(i64),
+}
+
+impl RegisterOrValue {
+    fn parse(rv: &str) -> Self {
+        let res = rv.parse::<i64>();
+        match res {
+            Ok(v) => RegisterOrValue::Value(v),
+            Err(_) => RegisterOrValue::Register(rv.parse::<char>().unwrap()),
+        }
+    }
+
+    fn get(&self, registers: &HashMap<char, i64>) -> i64 {
+        let val = match self {
+            RegisterOrValue::Register(r) => registers.get(r).unwrap(),
+            RegisterOrValue::Value(v) => v,
+        };
+        *val
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Instruction {
+    Set(char, RegisterOrValue),
+    Sub(char, RegisterOrValue),
+    Mul(char, RegisterOrValue),
+    Jnz(RegisterOrValue, i64),
+}
+
+impl Instruction {
+    fn parse(raw_input: &str) -> Vec<Self> {
+        let mut instructions = vec![];
+        for line in raw_input.lines() {
+            let (kind, infos) = line.split_once(" ").unwrap();
+            let i = match kind {
+                "set" => {
+                    let (x, y) = infos.split_once(" ").unwrap();
+                    Instruction::Set(x.parse::<char>().unwrap(), RegisterOrValue::parse(y))
+                }
+                "sub" => {
+                    let (x, y) = infos.split_once(" ").unwrap();
+                    Instruction::Sub(x.parse::<char>().unwrap(), RegisterOrValue::parse(y))
+                }
+                "mul" => {
+                    let (x, y) = infos.split_once(" ").unwrap();
+                    Instruction::Mul(x.parse::<char>().unwrap(), RegisterOrValue::parse(y))
+                }
+                "jnz" => {
+                    let (x, y) = infos.split_once(" ").unwrap();
+                    Instruction::Jnz(RegisterOrValue::parse(x), y.parse::<i64>().unwrap())
+                }
+                _ => unreachable!("Unknown instruction: '{kind}'!"),
+            };
+            instructions.push(i);
+        }
+        instructions
+    }
+}
+
+fn solve_part1(instructions: &[Instruction], registers: HashMap<char, i64>) -> i64 {
+    let mut nb_mul = 0;
+    let nb_instructions = instructions.len() as i64;
+    let mut position = 0;
+    let mut registers = registers.clone();
+    loop {
+        // Termination condition
+        if (position < 0) || (position >= nb_instructions) {
+            return nb_mul;
+        }
+
+        match &instructions[position as usize] {
+            Instruction::Set(r, rv) => {
+                let v = rv.get(&registers);
+                registers.insert(*r, v);
+                position += 1;
+            }
+            Instruction::Sub(r, rv) => {
+                let v = rv.get(&registers);
+                let previous_val = registers.get(r).unwrap();
+                registers.insert(*r, *previous_val - v);
+                position += 1;
+            }
+            Instruction::Mul(r, rv) => {
+                nb_mul += 1;
+                let v = rv.get(&registers);
+                let previous_val = registers.get(r).unwrap();
+                registers.insert(*r, *previous_val * v);
+                position += 1;
+            }
+            Instruction::Jnz(rv, v) => {
+                let check = rv.get(&registers);
+                if check != 0 {
+                    position += v;
+                } else {
+                    position += 1;
+                }
+            }
+        }
+    }
+}
+
+fn day23_part1(input: &[Instruction]) {
+    // Solve puzzle
+    let mut registers = HashMap::new();
+    registers.insert('a', 0i64);
+    registers.insert('b', 0i64);
+    registers.insert('c', 0i64);
+    registers.insert('d', 0i64);
+    registers.insert('e', 0i64);
+    registers.insert('f', 0i64);
+    registers.insert('g', 0i64);
+    registers.insert('h', 0i64);
+    let res = solve_part1(input, registers);
+    println!("Result part 1: {res}");
+    assert_eq!(res, 5929);
+    println!("> DAY23 - part 1: OK!");
+}
+
+fn day23_part2(_input: &[Instruction]) {
+    println!("TODO - part2");
+    // Exemple tests
+    // assert_eq!(, 0);
+    // println!("Example OK");
+
+    // Solve puzzle
+    // let res =
+    // println!("Result part 2: {res}");
+    // assert_eq!(res, );
+    // println!("> DAY23 - part 2: OK!");
+}
