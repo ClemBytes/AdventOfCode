2f8a549dc5e675550f94395bee12b5747c6ac1fe
Clémentine Scohy <clembytes@proton.me>
2025-12-04 22:29:34 +0100

    2025: DAY04: part2 OK (easy first try)

--- 2025/README.md
+++ 2025/README.md
@@ -55,5 +55,5 @@ cargo test --release -- --nocapture dayXX
 
 Progress so far:  
 ⭐⭐⭐⭐☆☆☆☆☆☆☆☆   
-⭐⭐⭐☆☆☆☆☆☆☆☆☆   
-**7 / 24 stars**
+⭐⭐⭐⭐☆☆☆☆☆☆☆☆   
+**8 / 24 stars**
--- 2025/src/day04.rs
+++ 2025/src/day04.rs
@@ -88,6 +88,44 @@ fn count_accessible_rolls(grid: &[Vec<char>]) -> usize {
     res
 }
 
+fn remove_accessible_rolls(grid: &[Vec<char>]) -> (usize, Vec<Vec<char>>) {
+    let mut grid_clone = grid.to_vec();
+    let mut nb_removed_rolls = 0;
+    let nb_rows = grid.len();
+    let nb_cols = grid[0].len();
+    for (i, line) in grid.iter().enumerate() {
+        for (j, &place) in line.iter().enumerate() {
+            if place == '@' {
+                let mut nb_adjacent_rolls = 0;
+                for pos in get_adjacent_coordinates(i, j, nb_rows, nb_cols) {
+                    if grid[pos.0][pos.1] == '@' {
+                        nb_adjacent_rolls += 1;
+                    }
+                }
+                if nb_adjacent_rolls < 4 {
+                    grid_clone[i][j] = '.';
+                    nb_removed_rolls += 1;
+                }
+            }
+        }
+    }
+    (nb_removed_rolls, grid_clone)
+}
+
+fn count_removable_rolls(grid: &[Vec<char>]) -> usize {
+    let mut grid = grid.to_vec();
+    let mut res = 0;
+    loop {
+        if count_accessible_rolls(&grid) == 0 {
+            return res;
+        }
+
+        let (nb_removed_rolls, grid_modified) = remove_accessible_rolls(&grid);
+        res += nb_removed_rolls;
+        grid = grid_modified;
+    }
+}
+
 fn day04_part1(example: &[Vec<char>], input: &[Vec<char>]) {
     // Exemple tests
     assert_eq!(count_accessible_rolls(example), 13);
@@ -99,15 +137,13 @@ fn day04_part1(example: &[Vec<char>], input: &[Vec<char>]) {
     println!("> DAY04 - part 1: OK!");
 }
 
-fn day04_part2(_example: &[Vec<char>], _input: &[Vec<char>]) {
-    println!("TODO - part2");
+fn day04_part2(example: &[Vec<char>], input: &[Vec<char>]) {
     // Exemple tests
-    // assert_eq!(, 0);
-    // println!("Example OK");
+    assert_eq!(count_removable_rolls(example), 43);
 
     // Solve puzzle
-    // let res =
-    // println!("Result part 2: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY04 - part 2: OK!");
+    let res = count_removable_rolls(input);
+    println!("Result part 2: {res}");
+    assert_eq!(res, 8643);
+    println!("> DAY04 - part 2: OK!");
 }
