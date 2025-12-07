7ea8832ff9bb198b4ce1f9a92d7ca90cb888fee3
Clémentine Scohy <clembytes@proton.me>
2025-12-07 18:28:21 +0100

    2025: DAY08: init

---
+++ 2025/src/day08.rs
@@ -0,0 +1,52 @@
+use std::fs;
+
+#[test]
+fn test() {
+    run();
+}
+
+pub fn run() {
+    println!("------- DAY08 -------");
+    let example = fs::read_to_string("inputs/example_day08").expect("Unable to read input!");
+    let example = parse(&example);
+    let input = fs::read_to_string("inputs/input_day08").expect("Unable to read input!");
+    let input = parse(&input);
+
+    day08_part1(&example, &input);
+    day08_part2(&example, &input);
+}
+
+fn parse(raw_input: &str) -> Vec<_> {
+    let mut yyy: Vec<_> = vec![];
+    for _line in raw_input.lines() {
+        // TODO
+        // yyy.push(line);
+    }
+    yyy
+}
+
+fn day08_part1(_example: &Vec<_>, _input: &Vec<_>) {
+    println!("TODO - part1");
+    // Exemple tests
+    // assert_eq!(, 0);
+    // println!("Example OK");
+
+    // Solve puzzle
+    // let res = 
+    // println!("Result part 1: {res}");
+    // assert_eq!(res, );
+    // println!("> DAY08 - part 1: OK!");
+}
+
+fn day08_part2(_example: &Vec<_>, _input: &Vec<_>) {
+    println!("TODO - part2");
+    // Exemple tests
+    // assert_eq!(, 0);
+    // println!("Example OK");
+
+    // Solve puzzle
+    // let res = 
+    // println!("Result part 2: {res}");
+    // assert_eq!(res, );
+    // println!("> DAY08 - part 2: OK!");
+}
