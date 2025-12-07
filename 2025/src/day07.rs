d35e0dea509643238d8db8c40571efc43b6aa09b
Clémentine Scohy <clembytes@proton.me>
2025-12-07 18:03:27 +0100

    2025: DAY07: part1 OK

--- 2025/src/day07.rs
+++ 2025/src/day07.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 #[test]
 fn test() {
@@ -8,44 +8,59 @@ fn test() {
 pub fn run() {
     println!("------- DAY07 -------");
     let example = fs::read_to_string("inputs/example_day07").expect("Unable to read input!");
-    let example = parse(&example);
     let input = fs::read_to_string("inputs/input_day07").expect("Unable to read input!");
-    let input = parse(&input);
 
-    day07_part1(&example, &input);
-    day07_part2(&example, &input);
+    day07_part1(example.clone(), input.clone());
+    day07_part2(example, input);
 }
 
-fn parse(raw_input: &str) -> Vec<_> {
-    let mut yyy: Vec<_> = vec![];
-    for _line in raw_input.lines() {
-        // TODO
-        // yyy.push(line);
+fn count_splits(diagram: String) -> usize {
+    let mut nb_splits = 0;
+    let mut current_beams = HashSet::new();
+    for line in diagram.lines() {
+        let mut new_beams = HashSet::new();
+        for (x, c) in line.chars().enumerate() {
+            // First line with source S
+            if c == 'S' {
+                new_beams.insert(x);
+                break;
+            }
+
+            // Other lines
+            if current_beams.contains(&x) {
+                if c == '.' {
+                    new_beams.insert(x);
+                } else if c == '^' {
+                    new_beams.insert(x - 1);
+                    new_beams.insert(x + 1);
+                    nb_splits += 1;
+                }
+            }
+        }
+        current_beams = new_beams;
     }
-    yyy
+    nb_splits
 }
 
-fn day07_part1(_example: &Vec<_>, _input: &Vec<_>) {
-    println!("TODO - part1");
+fn day07_part1(example: String, input: String) {
     // Exemple tests
-    // assert_eq!(, 0);
-    // println!("Example OK");
+    assert_eq!(count_splits(example), 21);
 
     // Solve puzzle
-    // let res = 
-    // println!("Result part 1: {res}");
-    // assert_eq!(res, );
-    // println!("> DAY07 - part 1: OK!");
+    let res = count_splits(input);
+    println!("Result part 1: {res}");
+    assert_eq!(res, 1711);
+    println!("> DAY07 - part 1: OK!");
 }
 
-fn day07_part2(_example: &Vec<_>, _input: &Vec<_>) {
+fn day07_part2(_example: String, _input: String) {
     println!("TODO - part2");
     // Exemple tests
     // assert_eq!(, 0);
     // println!("Example OK");
 
     // Solve puzzle
-    // let res = 
+    // let res =
     // println!("Result part 2: {res}");
     // assert_eq!(res, );
     // println!("> DAY07 - part 2: OK!");
