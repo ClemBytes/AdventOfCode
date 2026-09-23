53054d6e3e03bb79c5ee1d96f4186aa25e590890
Clémentine Scohy <clembytes@proton.me>
2026-09-23 18:35:48 +0200

    2018: day01: part2 OK

--- 2018/README.md
+++ 2018/README.md
@@ -55,5 +55,5 @@ cargo test --release -- --nocapture dayXX
 
 Progress so far:   
 ★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆   
-☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆   
-**1 / 50 stars**
+★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆   
+**2 / 50 stars**
--- 2018/src/day01.rs
+++ 2018/src/day01.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs;
 
 #[test]
@@ -11,7 +12,7 @@ pub fn run() {
     let input = parse(&input);
 
     day01_part1(&input);
-    // day01_part2(&example, &input);
+    day01_part2(&input);
 }
 
 fn parse(raw_input: &str) -> Vec<i32> {
@@ -23,23 +24,46 @@ fn parse(raw_input: &str) -> Vec<i32> {
     frequency_changes
 }
 
-fn day01_part1(input: &Vec<i32>) {
+fn day01_part1(input: &[i32]) {
     // Solve puzzle
-    let res: i32 = input.into_iter().sum();
+    let res: i32 = input.iter().sum();
     println!("Result part 1: {res}");
     assert_eq!(res, 510);
     println!("> DAY01 - part 1: OK!");
 }
 
-// fn day01_part2(_example: &Vec<_>, _input: &Vec<_>) {
-//     println!("TODO - part2");
-//     // Exemple tests
-//     // assert_eq!(, 0);
-//     // println!("Example OK");
-//
-//     // Solve puzzle
-//     // let res =
-//     // println!("Result part 2: {res}");
-//     // assert_eq!(res, );
-//     // println!("> DAY01 - part 2: OK!");
-// }
+fn found_twice(input: &[i32]) -> i32 {
+    let input_len = input.len();
+    let mut frequencies_reached: HashSet<i32> = HashSet::new();
+    frequencies_reached.insert(0);
+    let mut current_index = 0;
+    let mut current_frequency = 0;
+    loop {
+        current_frequency += input[current_index];
+        if frequencies_reached.contains(&current_frequency) {
+            return current_frequency;
+        }
+        frequencies_reached.insert(current_frequency);
+        current_index += 1;
+        current_index %= input_len;
+    }
+}
+
+fn day01_part2(input: &[i32]) {
+    // Exemple tests
+    let example: &[i32] = &[1, -1];
+    assert_eq!(found_twice(example), 0);
+    let example: &[i32] = &[3, 3, 4, -2, -4];
+    assert_eq!(found_twice(example), 10);
+    let example: &[i32] = &[-6, 3, 8, 5, -6];
+    assert_eq!(found_twice(example), 5);
+    let example: &[i32] = &[7, 7, -2, -7, -4];
+    assert_eq!(found_twice(example), 14);
+    println!("Example OK");
+
+    // Solve puzzle
+    let res = found_twice(input);
+    println!("Result part 2: {res}");
+    assert_eq!(res, 69074);
+    println!("> DAY01 - part 2: OK!");
+}
